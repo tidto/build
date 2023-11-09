8a0f1519dd787c6cb7f1b026c72b64eae45fa605
tidto <itnttoremayrain@gmail.com>
2023-11-09 10:53:41 +0900

    controller 수정

--- src/main/java/com/cocoball/app/CustomerController.java
+++ src/main/java/com/cocoball/app/CustomerController.java
@@ -4,6 +4,7 @@ package com.cocoball.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cocoball.app.accessingdatajpa.Customer;
 import com.cocoball.app.accessingdatajpa.CustomerRepository;
@@ -13,6 +14,13 @@ public class CustomerController {
 	
 	@Autowired
 	CustomerRepository repo;
+	
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>안</b>녕하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> findall(){
